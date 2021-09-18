e8bcdd74dd28d2f3499d469da6d33525da12aa3b
MrRio15 <89155983+MrRio15@users.noreply.github.com>
2021-09-18 20:06:42 +0300

    Add files via upload

---
+++ Task9.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Task9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        if (number % 2 == 0) System.out.println("Число четное");
+            else System.out.println("Число нечетное");
+    }
+}
